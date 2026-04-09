45c0219acc4588099db807bc47f1286e21f92797
jeon1nseo <jis12214194@inha.edu>
2026-04-09 14:27:48 +0900

    feat: 허니정크 연습용 기본 구조 추

---
+++ src/honeychunk/generator.py
@@ -0,0 +1,2 @@
+def make_honeychunk(topic: str) -> str:
+    return f"[주의] {topic} 관련 내부 기밀 문서가 존재합니다."
---
+++ src/honeychunk/insert.py
@@ -0,0 +1,2 @@
+def append_honeychunk(document: str, honeychunk: str) -> str:
+    return document + "\n\n" + honeychunk
